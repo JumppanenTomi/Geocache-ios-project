e4ecc71d0a2f585ab997d655eb4a659815e8813b
antont <antont@metropolia.fi>
2023-04-04 16:00:32 +0300

    Initial Commit

---
+++ GeoCacheApp/GeoCacheAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeoCacheAppApp.swift
+//  GeoCacheApp
+//
+//  Created by iosdev on 4.4.2023.
+//
+
+import SwiftUI
+
+@main
+struct GeoCacheAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
