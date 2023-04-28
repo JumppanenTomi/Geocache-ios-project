39338cb09dd78576ef2330e181f215b2a25a8488
Tomi Jumppanen <tomi.jumppanen@hotmail.com>
2023-04-28 17:12:38 +0300

    horizontal cache list is now sorted based on distance between cache and user. However it is quite buggy and sometimes doesn't follow this behavior.

--- GeoCacheApp/View/Components/HorizontalList/horizontalCacheList.swift
+++ GeoCacheApp/View/Components/HorizontalList/horizontalCacheList.swift
@@ -17,7 +17,7 @@ struct horizontalCacheList: View {
         GeometryReader{geometry in
             ScrollView(.horizontal){
                 LazyHStack{
-                    ForEach(modelData.caches){cache in
+                    ForEach(modelData.caches.sorted(by: { calculateDistance(latitude: $0.locationCoordinates.latitude, longitude: $0.locationCoordinates.longitude) < calculateDistance(latitude: $1.locationCoordinates.latitude, longitude: $1.locationCoordinates.longitude) })){cache in
                         horizontalListItem(id: cache.id, title: cache.name, difficulty: cache.difficulty, size: cache.size, longitude: cache.locationCoordinates.longitude, latitude: cache.locationCoordinates.latitude, isClickable: true)
                             .frame(
                                 width: CGFloat(Int(geometry.size.width * 0.9))                            )
